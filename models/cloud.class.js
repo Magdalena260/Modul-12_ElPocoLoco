c3941784d95f0629f6a36c347644517ee84ed2e4
Magdalena260 <magdalena.laurisch@outlook.de>
2026-04-21 20:10:27 +0200

    Interval clouds

--- models/cloud.class.js
+++ models/cloud.class.js
@@ -1,12 +1,25 @@
 class cloud extends MovableObject {
 
-    constructor() {
+
+
+
+    y = 20;
+    width = 500;
+    height = 250;
+
+constructor() {
         super();
         this.loadImage('img/5_background/layers/4_clouds/1.png');
 
         this.x = Math.random() * 500;
-        this.y = 20;
-        this.width = 500;
-        this.height = 250;
+        this.animate();
     }
+
+    animate(){
+    setInterval (() => {
+    this.x -= 0.15;
+    },1000 / 60); 
+   
+    }
+
 }
