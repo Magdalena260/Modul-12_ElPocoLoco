6f1e7e1b993d557aa8bbae24e6f19a60fc1b3b70
Magdalena260 <magdalena.laurisch@outlook.de>
2026-04-21 15:39:02 +0200

    more video

---
+++ models/cloud.class.js
@@ -0,0 +1,12 @@
+class cloud extends MovableObject {
+
+    constructor() {
+        super();
+        this.loadImage('img/5_background/layers/4_clouds/1.png');
+
+        this.x = Math.random() * 500;
+        this.y = 20;
+        this.width = 500;
+        this.height = 250;
+    }
+}
