7e8a7e237d3a1f2bf65c085dd29274b8389ba970
Magdalena260 <magdalena.laurisch@outlook.de>
2026-04-23 18:52:41 +0200

    changes 3

--- models/character.class.js
+++ models/character.class.js
@@ -2,7 +2,7 @@ class Character extends MovableObject {
 
     height = 300;
     y = 130;
-    speed = 1.5; // kleiner = besser für Auto-Run
+    speed = 10;
 
     IMAGES_WALKING = [
         'img/2_character_pepe/2_walk/W-21.png',
@@ -26,24 +26,43 @@ class Character extends MovableObject {
         this.animate();
     }
 
+    move() {
+
+        if (this.world.keyboard.RIGHT) {
+            this.x += this.speed;
+        }
+
+        if (this.world.keyboard.LEFT) {
+            this.x -= this.speed;
+        }
+
+        if (this.world.keyboard.SPACE) {
+            this.jump();
+        }
+    }
+
     animate() {
 
-        // 🔥 AUTO RUN (wie Chicken)
+        // Bewegung + Steuerung
         setInterval(() => {
-            this.x += this.speed;
+            this.move();
+            this.playAnimation();
         }, 1000 / 60);
+    }
 
-        // 🔥 WALK ANIMATION
-        setInterval(() => {
+    playAnimation() {
+
+        if (this.world.keyboard.RIGHT || this.world.keyboard.LEFT) {
 
             let i = this.currentImage % this.IMAGES_WALKING.length;
             let path = this.IMAGES_WALKING[i];
 
             this.img = this.imageCache[path];
             this.currentImage++;
-
-        }, 80);
+        }
     }
 
-    jump() {}
+    jump() {
+        // kann später Gravity bekommen
+    }
 }
